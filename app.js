dcf5edfbd08d9c30a17db975f30d878db767b95d
Diyor <diyor.islomov@gmail.com>
2015-06-25 18:56:53 +0100

    Angular frontend folder structure init

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/app/view/index.html');
+    res.sendFile(__dirname + '/public/app/views/index.html');
 });
 app.use('/api', api);
 
