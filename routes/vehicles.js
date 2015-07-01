7469d3117be703a939ed061e96115759e4ee0c43
Diyor <diyor.islomov@gmail.com>
2015-07-01 17:24:13 +0100

    Vehicle router for our express app added to handle /api/vehicle request path

---
+++ routes/vehicles.js
@@ -0,0 +1,22 @@
+﻿var express = require('express');
+var router = express.Router();
+
+var bookshelf = require('./bookshelfRTCMS.js');
+
+var Vehicle = bookshelf.Model.extend({
+    tableName: 'vehicle'
+});
+
+router.get('/', function (req, res) {
+    Vehicle.forge()
+    .fetchAll()
+    .then(function (vehicleCollection) {
+        res.json(vehicleCollection.toJSON());
+    }).catch(function (error) {
+        console.log(error);
+        res.send('An error occuered while retrieving vehicles data');
+    });
+});
+
+module.exports = router;
+
