8d5c1e6c89e4514e04db023f7b8ee620fedc04c3
Diyor <diyor.islomov@gmail.com>
2015-07-01 17:22:18 +0100

    custom bookshelf ORM working with RTCMS database has been created

---
+++ routes/bookshelfRTCMS.js
@@ -0,0 +1,17 @@
+﻿
+function Bookshelf() {
+    var knex = require('knex')({
+        client: 'mysql',
+        connection: {
+            host     : 'dbinstance1.cxlrzd9e5nxe.eu-central-1.rds.amazonaws.com',
+            user     : 'aws_master_diyor',
+            password : 'diyor2amazon',
+            database : 'procabdb',
+            charset  : 'utf8'
+        }
+    });
+    
+    return require('bookshelf')(knex);
+}
+
+module.exports = new Bookshelf();
