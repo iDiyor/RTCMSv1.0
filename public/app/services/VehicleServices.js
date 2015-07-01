bd69e915dbc543906db5883bc51ae15b25de0952
Diyor <diyor.islomov@gmail.com>
2015-07-01 17:21:16 +0100

    Vehicles tab added and correspoding services and controllers have been created to get data from vehicle table

---
+++ public/app/services/VehicleServices.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+/* Services */
+
+var vehicleServices = angular.module('vehicleServices', ['ngResources']);
+
+vehicleServices.factory('Vehicle', ['$resource', function ($resource) {
+        return $resource('api/:vehicleId', {} , {
+            query: { method: 'GET', params: { vehicleId: 'vehicles' }, isArray: true }
+        });
+}]);
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@
 var router = express.Router();
 
 var drivers = require('./drivers');
+var vehicles = require('./vehicles');
 
  //'/' is same as localhost:port/api/ 
  //if you type /api/ it will account as localhost:port/api/api <-- "/api/api"
@@ -10,5 +11,6 @@ router.get('/', function (req, res) {
 });
 
 router.use('/drivers', drivers);
+router.use('/vehicles', vehicles);
 
 module.exports = router;
