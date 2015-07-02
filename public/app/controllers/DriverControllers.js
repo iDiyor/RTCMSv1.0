ea59b1709d077036a1478728c6d89292a267a290
Diyor <diyor.islomov@gmail.com>
2015-07-02 15:16:54 +0100

    Driver templete, controllers and services added

---
+++ public/app/controllers/DriverControllers.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+
+/* Controllers */
+
+var driverControllers = angular.module('driverControllers', []);
+
+// Admin tab
+driverControllers.controller('DriverCtrl', ['$scope', 'Driver', 
+    function ($scope, Driver) {
+        $scope.drivers = Driver.query();
+        
+        $scope.title = 'Driver Tab';
+
+        $scope.onButtonClick = function (mode) {
+            console.log(mode + ' Button Clicked');
+            
+            
+            if (mode == 'edit') {
+                $('#editModalDialog').modal('show');
+            }
+            else if (mode == 'add') {
+                $('#addModalDialog').modal('show');
+            }
+
+            
+            console.log($scope.drivers);
+        };
+
+}]);
---
+++ public/app/services/DriverServices.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+/* Services */
+
+var adminServices = angular.module('driverServices', ['ngResource']);
+
+adminServices.factory('Driver', ['$resource', function ($resource) {
+        return $resource('api/:driverId', {}, {
+            query: { method: 'GET', params: { driverId: 'drivers' }, isArray: true }
+        });
+    }]);
+
+
+
+
+//create a service
+// add the service to the app
+// try to make http requests and get response (json)
+// table 
