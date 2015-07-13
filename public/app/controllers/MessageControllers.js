73662eb92a12dd5764e42cf8e9a32a77f6be96ca
Diyor <diyor.islomov@gmail.com>
2015-07-13 03:41:49 +0100

    Amazon server public IP congigured for Socket.io server

--- public/app/controllers/MessageControllers.js
+++ public/app/controllers/MessageControllers.js
@@ -10,7 +10,7 @@ messageControllers.controller('MessageCtrl', ['$scope', function ($scope) {
 
         console.log('MessageControllers');    
 
-    var socket = io.connect('http://localhost:1337');
+    var socket = io.connect('http://52.28.143.209:3000/');
     socket.on('news', function (data) {
         console.log(data);
         socket.emit('my other event', {my: 'data'});
