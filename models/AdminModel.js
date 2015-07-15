d9722ff3653a194084ff86149b2a1618a46cc290
Diyor <diyor.islomov@gmail.com>
2015-07-15 05:55:25 +0100

    Admin bookshelf.js model added to work with admin table in the database

---
+++ models/AdminModel.js
@@ -0,0 +1,12 @@
+﻿'use strict'
+
+var bookshelf = require('./BookshelfConnector.js');
+bookshelf.plugin('registry');
+
+var Admin = bookshelf.Model.extend({
+    tableName: 'admin',
+    idAttribute: 'id_admin'
+
+});
+
+module.exports = bookshelf.model('Admin', Admin);
