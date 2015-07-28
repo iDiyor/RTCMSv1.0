be114f0e644b0569d5291ff802d7b2672706dd24
Diyor <diyor.islomov@gmail.com>
2015-07-28 15:25:51 +0100

    Bug fixes

--- public/app/controllers/MapControllers.js
+++ public/app/controllers/MapControllers.js
@@ -122,6 +122,7 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', 'Socket', function (
                     geolocation.set('heading', degToRad(clientData.bearing));
                     // TODO geolocation service same as openlayers -> with changed, on, ... events
                     geolocation.changed();
+                    $scope.$emit('LocationUpdate', clientData.user);
                     
                     
 
@@ -130,23 +131,25 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', 'Socket', function (
                     //console.log(geolocation);
                 }
             }
-            $scope.$emit('LocationUpdate');
+            //$scope.$emit('LocationUpdate');
         });
         
-        $scope.$on('LocationUpdate', function () {
+        $scope.$on('LocationUpdate', function (data) {
             // line below never called
-            console.log('LOCATION UPDATE EVENT');
-            if (userGeolocationDataArray.length > 0 && userLocationMarkersArray.length > 0) {
-                for (var i = 0; i < userGeolocationDataArray.length; i++) {
-                    var userGeolocationObject = userGeolocationDataArray[i];
-                    for (var j = 0; j < userLocationMarkersArray.length; j++) {
-                        var userLocationMarkerObject = userLocationMarkersArray[j];
-                        if (userGeolocationObject.user == userLocationMarkerObject.user) {
-                            console.log('GEOLOCATION NAME MATCH');
-                        }
-                    }
-                }
-            }
+            console.log(data);
+            //if (userGeolocationDataArray.length > 0 && userLocationMarkersArray.length > 0) {
+            //    for (var i = 0; i < userGeolocationDataArray.length; i++) {
+            //        var userGeolocationObject = userGeolocationDataArray[i];
+            //        for (var j = 0; j < userLocationMarkersArray.length; j++) {
+            //            var userLocationMarkerObject = userLocationMarkersArray[j];
+            //            if (userGeolocationObject.user == userLocationMarkerObject.user) {
+            //                console.log('GEOLOCATION NAME MATCH');
+            //            }
+            //        }
+            //    }
+            //}
+
+
             //if (userGeolocationDataArray.length > 0 && userLocationMarkersArray.length > 0) {
             //    for (var i = 0; i < userGeolocationDataArray.length; i++) {
             //        var userGeolocationObject = userGeolocationDataArray[i];
