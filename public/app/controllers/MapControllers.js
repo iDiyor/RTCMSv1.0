8efbb9f6bb04e4e63db8f1b2d46dbce49c24f5ce
Diyor <diyor.islomov@gmail.com>
2015-07-29 16:49:15 +0100

    Bug fix

--- public/app/controllers/MapControllers.js
+++ public/app/controllers/MapControllers.js
@@ -218,7 +218,8 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', 'Socket', function (
             //var popupContent = $(popup).find('#popup-content').html('<p>' + clientData.name + '</p>');
             //var marker = $('#marker');
             //var locationMarkerIcon = $('.location_marker').clone(true, true); // clone(true) -> fixes click event on icon 
-            var locationMarkerIcon = $('<img class="location_marker" src="/images/cab-icon.png" data-toggle="popover" title="Info" data-content="" data-placement="top" />').appendTo('#location_marker_group'); // 
+            //var locationMarkerIcon = $('<img class="location_marker" src="/images/cab-icon.png" data-toggle="popover" title="Info" data-content="" data-placement="top" />').appendTo('#location_marker_group'); // 
+            var locationMarkerIcon = $('#location_marker_group').append('<img class="location_marker" src="/images/cab-icon.png" data-toggle="popover" title="Info" data-content="" data-placement="top" />');
             var overlay = new ol.Overlay({
                 element: locationMarkerIcon,
                 positioning: 'bottom-center'
