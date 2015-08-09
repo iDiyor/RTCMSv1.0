13b9192c37f8cb07a30b7c7fcf892a0751081480
Diyor <diyor.islomov@gmail.com>
2015-08-09 15:27:20 +0100

    Bug fix

--- public/app/controllers/MapControllers.js
+++ public/app/controllers/MapControllers.js
@@ -8,9 +8,7 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', function ($scope, Lo
         
         $scope.vehiclesNumberOnMap = 0;  
                 
-        // array to store icons/markers on the map
-        var clientLocationMarkersArray = [];
-        var clientGeolocationDataArray = [];
+        // client, overlay, geolocation
         var clientsArray = [];
         
         // convert radians to degrees
@@ -53,17 +51,7 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', function ($scope, Lo
             };
             
             socket.emit('client:connection', client);
-        });
-        
-        socket.on('disconnect', function () {
-            var client = {
-                name: 'jeb',
-                type: 'web'
-            };
-            
-            socket.emit('client:disconnect', client);
-        });
-            
+        });           
         
         /* SOCKET EVENT HANDLERS */
         // connection status from the server
@@ -75,46 +63,7 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', function ($scope, Lo
         
         // location data from the server (sent by mobile to the server => MOBILE-->SERVER-->WEB        
         socket.on('server:location', onServerLocation);
-        
-        
-        //Socket.On('server:location', function (clientData) {
-        //    /**
-        //     * clientData.clientId
-        //     * clientData.client 
-        //     * clientData.longitude
-        //     * clientData.latitude
-        //     * clientData.accuracy
-        //     * clientData.bearing
-        //     * clientData.speed
-        //     * clientData.time
-        //    */
-                        
-        //    for (var i = 0; i < clientsArray.length; i++) {
-        //        /**
-        //         * ClientsArray
-        //         * -> client
-        //         * -> overlay
-        //         * -> geolocation
-        //         */
-        //        var client = clientsArray[i].client;
-        //        // find the client who sent location from the array and update
-        //        if (client.id == clientData.clientId) {
-        //            //console.log('GEOLOCATION NAME MATCH');
-        //            var mobileLocation = [clientData.longitude, clientData.latitude];
-        //            var projectedLocation = ol.proj.transform(mobileLocation, 'EPSG:4326', 'EPSG:3857');
-
-        //            var geolocation = clientsArray[i].geolocation;
-        //            geolocation.set('position', projectedLocation);
-        //            geolocation.set('accuracy', clientData.accuracy);
-        //            geolocation.set('speed', clientData.speed);
-        //            geolocation.set('heading', degToRad(clientData.bearing));
-        //            // TODO geolocation service same as openlayers -> with changed, on, ... events
-        //            geolocation.changed();
-        //            $scope.$emit('LocationUpdate', { clientId: clientData.clientId });
-        //        }
-        //    }
-        //});
-        
+                
         function onServerLocation(clientData) {
             /**
              * clientData.clientId
@@ -177,76 +126,25 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', function ($scope, Lo
         socket.on('server:mobile:client:status', onServerMobileClientsStatus);
         
         function onServerMobileClientsStatus(clientData) {
-            
+            /**
+             * clientData.clientId
+             * clientData.client
+             * clientData.clientStatus
+             */ 
+
+            console.log(clientData);
+            for (var i = 0; i < clientsArray.length; i++) {
+                var client = clientsArray[i].client;
+                if (clientData.clientId == client.id) {
+                    client.status = clientData.clientStatus;
+                }
+            }
         }
 
         // on mobile connection event from the server 
-        // creates a popup for this mobile
-        
+        // creates a popup for this mobile        
         socket.on('server:mobile:connection', onServerMobileConnection);
-        //Socket.On('server:mobile:connection', function (clientData) {
-            
-        //    console.log(clientData);
-            
-        //    /**
-        //    * clientData.clientData
-        //    * -> client ID: String - ID ;
-        //    * -> client: String - client name(name of the driver) 
-        //    * -> last known location: Location
-        //    * -> type: String - mobile
-        //    */
-        //   var client = clientData.clientData;
-
-        //   var clientObject = {
-        //        id: client.clientId,
-        //        name: client.client,
-        //        status: 'No Job'
-        //    }
-
-        //    createMarkerAndGeolocationForEachClient(clientObject);
-        //    setGeolocation(clientObject, client.lastKnowLocation);
-
-
-        //    //var marker = $('<img class="location_marker" src="/images/cab-icon.png" data-toggle="popover" title="Info" data-content="" data-placement="top" />');
-        //    //marker.click(onClientMarkerClick);
-        //    //var locationMarkerIcon = marker.appendTo('.location_marker_group');
-
-        //    //var overlay = new ol.Overlay({
-        //    //    element: locationMarkerIcon,
-        //    //    positioning: 'bottom-center'
-        //    //});
-            
-        //    //// name should be unique for each connected device -> id of the driver
-        //    //var clientLocationMarkerObject = { client: clientData.client , overlay: overlay };
-     
-        //    //// adding new overlay into the array
-        //    //map.addOverlay(overlay);
-        //    //// adding new overlay on the map to make it visible
-        //    //clientLocationMarkersArray.push(clientLocationMarkerObject);
-            
-        //    //// client geolocation data
-        //    //var geolocation = new ol.Geolocation({
-        //    //    projection: view.getProjection(),      
-        //    //    trackingOptions: {
-        //    //        maximumAge: 10000,
-        //    //        enableHighAccuracy: true,
-        //    //        timeout: 600000
-        //    //    }
-        //    //});
-        //    ////geolocation.set("client", clientData.client);
-        //    //var clientGeolocationObject = { client: clientData.client, geolocation: geolocation };
-
-        //    //// adding new client geolocation var into the array
-        //    //clientGeolocationDataArray.push(clientGeolocationObject);
-            
-        //    //console.log('mobile connect');
-        //    //console.log('Devices online: ' + clientLocationMarkersArray.length.toString());           
-        //    //console.log('Geolocation online: ' + clientGeolocationDataArray.length.toString());
-
-        //    //$scope.vehiclesNumberOnMap = clientGeolocationDataArray.length;
-            
-        //});
-        
+               
         function onServerMobileConnection(clientData) {
             console.log(clientData);
             
@@ -270,34 +168,7 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', function ($scope, Lo
         }
         
         socket.on('server:online:mobile:clients', onServerOnlineMobileClients);
-        
-        //Socket.On('server:online:mobile:clients', function (mobileClients) {
-            
-        //    console.log(mobileClients);
-        //    /**
-        //     * mobileClients
-        //     * -> client ID: String - ID ;
-        //     * -> client: String - client name(name of the driver) 
-        //     * -> last known location: Location
-        //     * -> type: String - mobile
-        //     */ 
-        //    for (var i = 0; i < mobileClients.length; i++) {
-        //        var client = mobileClients[i].clientData;
-        //        // client.clientId
-        //        // client.client
-        //        // client.lastKnowLocation -> .latitude .longitude .accuracy ....
-        //        // type: mobile
-        //        var clientObject = {
-        //            id: client.clientId,
-        //            name: client.client,
-        //            status: 'No Job'
-        //        }
-        //        createMarkerAndGeolocationForEachClient(clientObject);
-        //        setGeolocation(clientObject, client.lastKnowLocation);
-        //    }
-
-        //});
-        
+                
         function onServerOnlineMobileClients(mobileClients) {
             console.log(mobileClients);
             /**
@@ -324,37 +195,8 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', function ($scope, Lo
         };
 
         // on mobile disconnection event from the server
-
         socket.on('server:mobile:disconnection', onServerMobileDisconnection);
-        //Socket.On('server:mobile:disconnection', function (clientData) {
-        //    /**
-        //     * clientData.type
-        //     * clientData.clientId
-        //     * clientData.client
-        //     */
-            
-        //    // get the name of the disconnected device and remove from the array using that name
-        //    var clientId = clientData.clientId;
-            
-        //    if (clientsArray.length > 0) {
-        //        for (var i = 0; i < clientsArray.length; i++) {
-        //            var client = clientsArray[i].client;
-        //            if (clientId == client.id) {
-        //                var overlay = clientsArray[i].overlay;
-                        
-        //                map.removeOverlay(overlay);
 
-        //                clientsArray.splice(i, 1);
-        //            }   
-        //        }  
-        //    }
-          
-        //    //console.log('Client Location Marker #: ' + clientLocationMarkersArray.length.toString());
-        //    //console.log('Client Geolocation #: ' + clientGeolocationDataArray.length.toString());
-        //    console.log('Clients #: ' + clientsArray.length);
-        //    $scope.vehiclesNumberOnMap = clientsArray.length;
-        //});
-        
         function onServerMobileDisconnection(clientData) {
             /**
              * clientData.type
@@ -452,10 +294,6 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', function ($scope, Lo
             
             clientsArray.push(clientObject);
             
-
-            console.log('mobile connect');
-            //console.log('Devices online: ' + clientLocationMarkersArray.length.toString());
-            //console.log('Geolocation online: ' + clientGeolocationDataArray.length.toString());
             console.log('Clients #' + clientsArray.length);
             $scope.vehiclesNumberOnMap = clientsArray.length;
         }
@@ -480,7 +318,7 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', function ($scope, Lo
             }
         }
 
-        $scope.$on('$destroy', function iVeBeenDismissed() {
+        $scope.$on('$destroy', function onMapControllerDestroy() {
             // say goodbye to your controller here
             // release resources, cancel request...
             console.log("MapController destroyed");
