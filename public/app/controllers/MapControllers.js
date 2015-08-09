2ce7b7b3fbe3e0313f6b761db0fe6b860153f780
Diyor <diyor.islomov@gmail.com>
2015-08-09 16:37:56 +0100

    Bug fix

--- public/app/controllers/MapControllers.js
+++ public/app/controllers/MapControllers.js
@@ -244,7 +244,7 @@ mapControllers.controller('MapCtrl', ['$scope', 'Location', function ($scope, Lo
                     $(overlay.getElement()).popover({
                         html: true,
                         container: 'body',
-                        trigger: 'manual',
+                        trigger: 'click',
                         content: client.status + '<div><p style="display: inline">Name: ' + '<b>' + clientsArray[i].client.name + '</b></p><br><p>Status: ' + '<b>' + client.status +'</b></p></div>'
                     });
                     
